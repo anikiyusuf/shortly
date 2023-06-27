6471b6b4070e1c834020063b82387f8131c634c2
Aniki Yusuf <anikiyusuf99@gmail.com>
2023-06-27 20:23:38 +0100

    initial commit

--- routes/urlRoutes.js
+++ routes/urlRoutes.js
@@ -11,148 +11,148 @@ function validateRequest(req, res, next) {
   next();
 }
 
-/**
- * @swagger
- * /url:
- *   get:
- *     summary: Getting all URLs.
- *     description: Retrieve a list of URLs.
- *     responses:
- *       200:
- *         description: URLs.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   type: array
- *                   items:
- *                     type: object
- *                     properties:
- *                       id:
- *                         type: string
- *                         description: The URL ID.
- *                         example: 472424sdvnsef2rv87
- *                       full:
- *                         type: string
- *                         description: The long URL.
- *                         example: https://shortly-y.onrender.com
- *                       short:
- *                         type: string
- *                         description: The short URL.
- *                         example: 3jee4890
- *                       click:
- *                         type: number
- *                         description: The number of clicks.
- *                         example: 2
- */
+// /**
+//  * @swagger
+//  * /url:
+//  *   get:
+//  *     summary: Getting all URLs.
+//  *     description: Retrieve a list of URLs.
+//  *     responses:
+//  *       200:
+//  *         description: URLs.
+//  *         content:
+//  *           application/json:
+//  *             schema:
+//  *               type: object
+//  *               properties:
+//  *                 data:
+//  *                   type: array
+//  *                   items:
+//  *                     type: object
+//  *                     properties:
+//  *                       id:
+//  *                         type: string
+//  *                         description: The URL ID.
+//  *                         example: 472424sdvnsef2rv87
+//  *                       full:
+//  *                         type: string
+//  *                         description: The long URL.
+//  *                         example: https://shortly-y.onrender.com
+//  *                       short:
+//  *                         type: string
+//  *                         description: The short URL.
+//  *                         example: 3jee4890
+//  *                       click:
+//  *                         type: number
+//  *                         description: The number of clicks.
+//  *                         example: 2
+//  */
 router.get('/url', getShortUrls);
 
-/**
- * @swagger
- * /shortUrls:
- *   post:
- *     summary: Creating a URL.
- *     description: Create a short URL.
- *     responses:
- *       200:
- *         description: URL.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   type: array
- *                   items:
- *                     type: object
- *                     properties:
- *                       id:
- *                         type: string
- *                         description: The URL ID.
- *                         example: 472424sdvnsef2rv87
- *                       full:
- *                         type: string
- *                         description: The long URL.
- *                         example: https://shortly-y.onrender.com
- *                       short:
- *                         type: string
- *                         description: The short URL.
- *                         example: 3jee4890
- *                       click:
- *                         type: number
- *                         description: The number of clicks.
- *                         example: 0
- */
-router.post('/shortUrls', validateRequest, createShortUrl);
+// // /**
+// //  * @swagger
+// //  * /shortUrls:
+// //  *   post:
+// //  *     summary: Creating a URL.
+// //  *     description: Create a short URL.
+// //  *     responses:
+// //  *       200:
+// //  *         description: URL.
+// //  *         content:
+// //  *           application/json:
+// //  *             schema:
+// //  *               type: object
+// //  *               properties:
+// //  *                 data:
+// //  *                   type: array
+// //  *                   items:
+// //  *                     type: object
+// //  *                     properties:
+// //  *                       id:
+// //  *                         type: string
+// //  *                         description: The URL ID.
+// //  *                         example: 472424sdvnsef2rv87
+// //  *                       full:
+// //  *                         type: string
+// //  *                         description: The long URL.
+// //  *                         example: https://shortly-y.onrender.com
+// //  *                       short:
+// //  *                         type: string
+// //  *                         description: The short URL.
+// //  *                         example: 3jee4890
+// //  *                       click:
+// //  *                         type: number
+// //  *                         description: The number of clicks.
+// //  *                         example: 0
+// //  */
+ router.post('/shortUrls', validateRequest, createShortUrl);
 
-/**
- * @swagger
- * /:shortUrl:
- *   get:
- *     summary: Redirecting to full URL.
- *     description: Redirect to the full URL.
- *     responses:
- *       200:
- *         description: URL.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   type: array
- *                   items:
- *                     type: object
- *                     properties:
- *                       id:
- *                         type: string
- *                         description: The URL ID.
- *                         example: 472424sdvnsef2rv87
- *                       full:
- *                         type: string
- *                         description: The long URL.
- *                         example: https://shortly-y.onrender.com
- *                       short:
- *                         type: string
- *                         description: The short URL.
- *                         example: 3jee4890
- *                       click:
- *                         type: number
- *                         description: The number of clicks.
- *                         example: 2
- */
-router.get('/:shortUrl', redirectToFullUrl);
+// // /**
+// //  * @swagger
+// //  * /:shortUrl:
+// //  *   get:
+// //  *     summary: Redirecting to full URL.
+// //  *     description: Redirect to the full URL.
+// //  *     responses:
+// //  *       200:
+// //  *         description: URL.
+// //  *         content:
+// //  *           application/json:
+// //  *             schema:
+// //  *               type: object
+// //  *               properties:
+// //  *                 data:
+// //  *                   type: array
+// //  *                   items:
+// //  *                     type: object
+// //  *                     properties:
+// //  *                       id:
+// //  *                         type: string
+// //  *                         description: The URL ID.
+// //  *                         example: 472424sdvnsef2rv87
+// //  *                       full:
+// //  *                         type: string
+// //  *                         description: The long URL.
+// //  *                         example: https://shortly-y.onrender.com
+// //  *                       short:
+// //  *                         type: string
+// //  *                         description: The short URL.
+// //  *                         example: 3jee4890
+// //  *                       click:
+// //  *                         type: number
+// //  *                         description: The number of clicks.
+// //  *                         example: 2
+// //  */
+ router.get('/:shortUrl', redirectToFullUrl);
 
-/**
- * @swagger
- * /generateqrcode:
- *   get:
- *     summary: Generate QR Code.
- *     description: Retrieve a QR code.
- *     responses:
- *       200:
- *         description: QR Code.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   type: array
- *                   items:
- *                     type: object
- *                     properties:
- *                       id:
- *                         type: string
- *                         description: The QR code ID.
- *                         example: 472424sdvnsef2rv87
- *                       full:
- *                         type: string
- *                         description: The long URL.
- *                         example: 253i9oteodmaqwrkm rmkamdqakeeqa
- */
+// /**
+//  * @swagger
+//  * /generateqrcode:
+//  *   get:
+//  *     summary: Generate QR Code.
+//  *     description: Retrieve a QR code.
+//  *     responses:
+//  *       200:
+//  *         description: QR Code.
+//  *         content:
+//  *           application/json:
+//  *             schema:
+//  *               type: object
+//  *               properties:
+//  *                 data:
+//  *                   type: array
+//  *                   items:
+//  *                     type: object
+//  *                     properties:
+//  *                       id:
+//  *                         type: string
+//  *                         description: The QR code ID.
+//  *                         example: 472424sdvnsef2rv87
+//  *                       full:
+//  *                         type: string
+//  *                         description: The long URL.
+//  *                         example: 253i9oteodmaqwrkm rmkamdqakeeqa
+//  */
 router.get('/generateqrcode', generateQRCode);
 
 module.exports = router;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,76 +3,76 @@ const { register, login } = require('../controllers/userController');
 
 const userRouter = express.Router();
 
-/**
- * @swagger
- * /signup:
- *   post:
- *     summary: Creating a user account.
- *     description: Get user details to register.
- *     responses:
- *       200:
- *         description: A single user.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   type: object
- *                   properties:
- *                     id:
- *                       type: integer
- *                       description: The user ID.
- *                       example: 6e3u142kmqmnwemqnwmn
- *                     firstName:
- *                       type: string
- *                       description: The user's firstName.
- *                       example: Yusuf
- *                     lastName:
- *                       type: string 
- *                       description: The user's lastName.
- *                       example: Aniki
- *                     email:
- *                       type: string
- *                       description: The user's email.
- *                       example: anikiyusuf99@gmail.com
- *                     password:
- *                       type: string
- *                       description: The user's password.
- *                       example: aniki12345
- */
+// /**
+//  * @swagger
+//  * /signup:
+//  *   post:
+//  *     summary: Creating a user account.
+//  *     description: Get user details to register.
+//  *     responses:
+//  *       200:
+//  *         description: A single user.
+//  *         content:
+//  *           application/json:
+//  *             schema:
+//  *               type: object
+//  *               properties:
+//  *                 data:
+//  *                   type: object
+//  *                   properties:
+//  *                     id:
+//  *                       type: integer
+//  *                       description: The user ID.
+//  *                       example: 6e3u142kmqmnwemqnwmn
+//  *                     firstName:
+//  *                       type: string
+//  *                       description: The user's firstName.
+//  *                       example: Yusuf
+//  *                     lastName:
+//  *                       type: string 
+//  *                       description: The user's lastName.
+//  *                       example: Aniki
+//  *                     email:
+//  *                       type: string
+//  *                       description: The user's email.
+//  *                       example: anikiyusuf99@gmail.com
+//  *                     password:
+//  *                       type: string
+//  *                       description: The user's password.
+//  *                       example: aniki12345
+//  */
 userRouter.post('/register', register);
 
-/**
- * @swagger
- * /login:
- *   post:
- *     summary: Login user.
- *     description: Login with user credentials.
- *     responses:
- *       200:
- *         description: A single user.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   type: object
- *                   properties:
- *                     id:
- *                       type: integer
- *                       description: The user ID.
- *                       example: 6e3u142kmqmnwemqnwmn
- *                     email:
- *                       type: string
- *                       description: The user's email.
- *                       example: anikiyusuf99@gmail.com
- *                     password:
- *                       type: string
- *                       description: The user's password.
- *                       example: aniki12345
- */
+// /**
+//  * @swagger
+//  * /login:
+//  *   post:
+//  *     summary: Login user.
+//  *     description: Login with user credentials.
+//  *     responses:
+//  *       200:
+//  *         description: A single user.
+//  *         content:
+//  *           application/json:
+//  *             schema:
+//  *               type: object
+//  *               properties:
+//  *                 data:
+//  *                   type: object
+//  *                   properties:
+//  *                     id:
+//  *                       type: integer
+//  *                       description: The user ID.
+//  *                       example: 6e3u142kmqmnwemqnwmn
+//  *                     email:
+//  *                       type: string
+//  *                       description: The user's email.
+//  *                       example: anikiyusuf99@gmail.com
+//  *                     password:
+//  *                       type: string
+//  *                       description: The user's password.
+//  *                       example: aniki12345
+//  */
 userRouter.post('/login', login);
 
 module.exports = userRouter;
